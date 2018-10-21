a0579011fdc4aaada86db3513dad356520b5bef4
Baecode <byp93@naver.com>
2018-10-22 00:37:12 +0900

    Weather Icon 추가

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { yellow } from 'ansi-colors';
+import React, { Componecnt } from 'react';
+import { StyleSheet, Text, View, StatusBar} from 'react-native';
+import Weather from "./Weather";
 
 export default class App extends React.Component {
+  state = {
+    isLoaded: true
+  }
   render() {
+    const { isLoaded } = this.state;
     return (
       <View style={styles.container}>
-      <View style={styles.redView}/>
-      <View style={styles.yellowView}/>
+      <StatusBar hidden={true}/>
+        {isLoaded ? <Weather /> : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>getting the Weather</Text>
+          </View>
+        )}
       </View>
     );
   }
@@ -17,18 +25,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems:'center'
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    flexDirection:'row',
+    flexWrap:'wrap'
 
   },
-  redView:{
-    height:50,
-    width:50,
-    backgroundColor:'red'
+  loading:{
+    flex : 1,
+      backgroundColor:'#FDF6AA',
+      justifyContent: 'flex-end',
+      paddingLeft:25
   },
-  yellowView:{
-    height:50,
-    width:50,
-    backgroundColor:'yellow'
+  loadingText:{
+    fontSize : 38,
+    marginBottom : 100,
   }
 });
